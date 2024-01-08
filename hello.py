da8b64456cbc83fdd76011e5d70336aedb304d2c
Sofia Neo <sofineo@gmail.com>
2024-01-07 21:50:54 -0800

    add SHEBANG and new comment

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+#SHABNAG - message sent to your OS before starting the file
+
+# This program print "Hello World"
+
+print("Hello, World!") #test-ignore
